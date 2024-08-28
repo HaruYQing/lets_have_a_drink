a63411ff17cb622c4957cf2408e4e3e4ee88378a
HaruYQing <haru61410@gmail.com>
2024-08-28 21:30:26 +0800

    fix err: No QueryClient set

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Outlet, BrowserRouter } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { useQuery } from "react-query";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import axios from "axios";
 import Landing from "./components/Landing";
 import OwnerForm from "./components/OwnerForm";
@@ -8,6 +8,7 @@ import EventDetail from "./components/EventDetail";
 
 import "./style.scss";
 import "./font/NaikaiFont-Regular.ttf";
+const queryClient = new QueryClient();
 
 function App() {
   const [eid, setEid] = useState(null);
@@ -17,20 +18,20 @@ function App() {
     setEid(newEid);
   };
 
-  const {
-    data: eventDetail,
-    error,
-    isLoading,
-  } = useQuery(
-    ["eventDetail", eid],
-    async () => {
-      const response = await axios.get(
-        `http://localhost:8000/showEvent/${eid}`
-      );
-      return response.data;
-    },
-    { enabled: !!eid }
-  );
+  // const {
+  //   data: eventDetail,
+  //   error,
+  //   isLoading,
+  // } = useQuery(
+  //   ["eventDetail", eid],
+  //   async () => {
+  //     const response = await axios.get(
+  //       `http://localhost:8000/showEvent/${eid}`
+  //     );
+  //     return response.data;
+  //   },
+  //   { enabled: !!eid }
+  // );
 
   // useEffect(() => {
   //   const fetchEventDetail = async () => {
@@ -48,22 +49,48 @@ function App() {
   // }, [eid]);
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="arrange" element={<OwnerForm handleEid={handleEid} />} />
-        {eventDetail ? (
-          <Route
-            path="details"
-            element={<EventDetail eid={eid} eventDetail={eventDetail} />}
-          />
-        ) : (
-          "Loading..."
-        )}
-      </Routes>
-      <Outlet />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="arrange" element={<OwnerForm handleEid={handleEid} />} />
+          {/* {eventDetail ? (
+            <Route
+              path="details"
+              // element={<EventDetail eid={eid} />}
+              element={<EventDetail eid={eid} eventDetail={eventDetail} />}
+            />
+          ) : (
+            "Loading..."
+          )} */}
+          <Route path="details" element={<EventDetailFetcher eid={eid} />} />
+        </Routes>
+        <Outlet />
+      </BrowserRouter>
+    </QueryClientProvider>
   );
+}
+
+function EventDetailFetcher({ eid }) {
+  const {
+    data: eventDetail,
+    error,
+    isLoading,
+  } = useQuery(
+    ["eventDetail", eid],
+    async () => {
+      const response = await axios.get(
+        `http://localhost:8000/showEvent/${eid}`
+      );
+      return response.data;
+    },
+    { enabled: !!eid }
+  );
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return <EventDetail eid={eid} eventDetail={eventDetail} />;
 }
 
 export default App;
